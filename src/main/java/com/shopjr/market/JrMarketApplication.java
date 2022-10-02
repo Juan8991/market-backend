50c6c1cbc5d12c5ba62bebf01d51f12443d18464
JuanR <ramirezjuan9806@gmail.com>
2022-10-02 09:25:24 -0500

    Primer commit probando funcionamiento de estructura proveeida por spring initializr

---
+++ src/main/java/com/shopjr/market/JrMarketApplication.java
@@ -0,0 +1,13 @@
+package com.shopjr.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopjr/market/JrMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopjr.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
